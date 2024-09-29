b1eb407dd0778716ea71f1be085671f34a7b4192
mediaswhatif <mediaswhatif@gmail.com>
2024-09-29 13:34:05 +0530

    ui integrated with carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,SlickCarouselModule],
   providers:[],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -22,54 +22,102 @@ export class HomeComponent {
       name: 'Alice Johnson',
       title: 'Frontend Developer',
       description: '“Working with them was an absolute pleasure! Their expertise in Angular is top-notch.”',
-      shrinkValue: 1 // Default flex-shrink value
     },
     {
       img: 'https://raw.githubusercontent.com/mustafadalga/farm-landing-page/master/assets/img/testimonials/2.png',
       name: 'Bob Smith',
       title: 'Backend Engineer',
       description: '“Their attention to detail and coding standards is impressive. Highly recommend!”',
-      shrinkValue: 1
     },
     {
       img: 'https://raw.githubusercontent.com/mustafadalga/farm-landing-page/master/assets/img/testimonials/3.png',
       name: 'John Doe',
       title: 'Software Engineer',
       description: '“I learned so much collaborating on projects. Their insights are invaluable.”',
-      shrinkValue: 1
+    },
+    {
+      img: 'https://raw.githubusercontent.com/mustafadalga/farm-landing-page/master/assets/img/testimonials/2.png',
+      name: 'Smriti',
+      title: 'Software Engineer',
+      description: '“I learned so much collaborating on projects. Their insights are invaluable.”',
     }
   ];
+  technologies: any[] = [
+    {
+      img: '/HTML5.png',
+      imageName: 'HTML5'
+    },
+    {
+      img: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg',
+      imageName: 'CSS3'
+    },
+    {
+      img: '/javascript.svg',
+      imageName: 'JavaScript'
+    },
+    {
+      img: 'https://www.capitalnumbers.com/images/technologies-icons/angular-icon.svg',
+      imageName: 'Angular'
+    },
+    {
+      img: 'https://www.capitalnumbers.com/images/technologies-icons/typescript-icon.svg',
+      imageName: 'TypeScript'
+    },
+    {
+      img: 'https://www.capitalnumbers.com/images/technologies-icons/nodejs-icon.svg',
+      imageName: 'Node.js'
+    },
+    {
+      img: 'https://www.capitalnumbers.com/images/technologies-icons/azure-icon.svg',
+      imageName: 'Azure'
+    }
+  ];
+  
   selectedIndex: number = 0;
+  slideConfig1 = {
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
-
-  // Adjust the positions of testimonials based on action
-// Adjust the positions of testimonials based on action
-adjustFlexShrink(action: string) {
-  if (action === 'increase' && this.selectedIndex > 0) {
-    // Swap with the previous testimonial
-    [this.testimonials[this.selectedIndex], this.testimonials[this.selectedIndex - 1]] = [
-      this.testimonials[this.selectedIndex - 1],
-      this.testimonials[this.selectedIndex]
-    ];
-    this.selectedIndex--; // Move selection up
-  } else if (action === 'decrease' && this.selectedIndex < this.testimonials.length - 1) {
-    // Swap with the next testimonial
-    [this.testimonials[this.selectedIndex], this.testimonials[this.selectedIndex + 1]] = [
-      this.testimonials[this.selectedIndex + 1],
-      this.testimonials[this.selectedIndex]
-    ];
-    this.selectedIndex++; // Move selection down
-  }
-  this.randomSelect()
-}
-
-randomSelect() {
-  this.selectedIndex = Math.floor(Math.random() * this.testimonials.length);
-}
-
-  // Set selected index for swapping
-  setSelectedIndex(index: number) {
-    this.selectedIndex = index;
-  }
+  slideConfig2 = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
   
 }
