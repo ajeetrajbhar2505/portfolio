ee7398e6a60b396e5256f5476340688f60beca41
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-10-01 00:35:07 +0530

    minor changes ui hinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'portfolo';
+export class AppComponent implements OnInit {
+  isLoading = true;
+
+  ngOnInit() {
+    document.addEventListener('DOMContentLoaded', () => {
+      this.isLoading = false;
+    });
+  }
 }
